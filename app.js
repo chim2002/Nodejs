14c8526091e91126ab1136e531a2c76bbc563fde
Induni Warnachinthaka <it23310278@my.sliit.lk>
2026-02-22 10:30:44 +0530

    Create basic web server

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello Indie - Node is working!");
+console.log("Hello Indie - Node is working!");const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
